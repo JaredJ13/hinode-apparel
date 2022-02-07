c5b2119a720d8463ecb8508dfa11ca3a20547b53
Jared Jahnke <77992735+IJaredI@users.noreply.github.com>
2022-02-07 15:41:39 -0700

    product card - added href to edit button

--- src/js/templates/productCard.js
+++ src/js/templates/productCard.js
@@ -14,7 +14,7 @@ function productCard({ imageUrlPath, title, description, price, category, key, i
             <div class="product-bottom-details">
                 <div class="product-price">$${price}</div>
                 <div class="product-links">
-                    <a>
+                    <a href="edit.html">
                         <svg id="edit" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" data-image = "${imageFileName}" data-key="${key}" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -47,7 +47,6 @@ function addButtonControls(productCard) {
 function EditProductCard(e) {
     const key = e.currentTarget.dataset.key
     sessionStorage.setItem('product-key', key)
-    window.location.assign('edit.html')
 }
 
 function DeleteModal(e) {
